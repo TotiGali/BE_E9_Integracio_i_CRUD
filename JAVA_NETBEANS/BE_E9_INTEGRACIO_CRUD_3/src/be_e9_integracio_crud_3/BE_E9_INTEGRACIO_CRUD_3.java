a8925d3f9d4921a030c20414953b05ae3b54a142
TotiGali <jordi@comoba.org>
2024-05-21 18:03:56 +0200

    segona actualització

--- JAVA_NETBEANS/BE_E9_INTEGRACIO_CRUD_3/src/be_e9_integracio_crud_3/BE_E9_INTEGRACIO_CRUD_3.java
+++ JAVA_NETBEANS/BE_E9_INTEGRACIO_CRUD_3/src/be_e9_integracio_crud_3/BE_E9_INTEGRACIO_CRUD_3.java
@@ -14,20 +14,31 @@ public class BE_E9_INTEGRACIO_CRUD_3 {
      * @param args the command line arguments
      */
     public static void main (String [] args){
-         User usuario1 = new User("user1","password1","user1@emial.com");
         
-        UserDAO.printUserDetails();
+        //CREEM USUARI 1 AMB UNA INSTÀNCIA DE LA CLASSE USER
+        //User usuario1 = new User("user1","password1","user1@emial.com");
+        //UserDAO.printUserDetails();
+        
+        //CREEM USUARI 2 DIRECTE
+        //UserDAO.insertUser("user2","password2","user2@emial.com");
+        
+        //CREEM USUARI 5 AMB INSTANCIA PER CRIDAR-LO I EDITAR EL MAIL A CONTINUACIÓ
+        //User usuario5 = new User("user5","password5","user5@email.com");
+        
+        //CANVIEM EL MAIL DE USUARI 5. IMPORTANT!!!! CAL FER-HO AL MATEIX TEMPS DE LA CREACIÓ usuario5 SI ES FA PER INSTANCIA
+        //usuario5.getEmail();
+        //usuario5.setEmail("nuevoemail5@gmail.com");
+        
+        /*ESBORREM USUARI 2 I EL TORNEM A CREAR AMB ELS METODES ANTERIORS
+        UserDAO.deleteUser(2,"user2");
+        System.out.println("Nueva lista con los usuarios actualizados: ");
+        */  
+        
+        //CANVI DE EMAIL DEL USER2
+        //UserDAO.getUserEmail("user2");
+        //UserDAO.updateUserEmail("user2","noumail@gmail.com");
+        
                 
-        UserDAO.insertUser("user2","password2","user2@emial.com");
-        
-        System.out.println("Nueva lista con los usuairos nuevos actualizados: ");
-      
-        // usuario1.getEmail();
-        //UserDAO.getUserEmail("Sergi10");        
-        // UserDAO.updateUserEmail(2, "usuario2nuevo@ejemplo.com");
-        //usuario1.setEmail("nuevoemailejemplo@gmail.com");
-        //UserDAO.updateUserEmail("Sergi", "jadlfjdjsk@email.com");
-        
         
     }
     
